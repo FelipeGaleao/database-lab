c4391d52fdb5789283473fe7930e67977f744493
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-08-15 22:08:46 -0300

    feat: adicionado script para criar a tabela filme

--- scripts/aula_03/locadora.sql
+++ scripts/aula_03/locadora.sql
@@ -16,3 +16,16 @@ CREATE TABLE ator (
     ator_dtnas DATE NOT NULL,
     PRIMARY KEY (ator_id)
 );
+
+-- Criar tabela Filme
+USE locadora;
+CREATE TABLE filme (
+    filme_cod INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    filme_titulo VARCHAR(256) NOT NULL,
+    filme_categoria VARCHAR(50) NOT NULL,
+    filme_ator VARCHAR(50) NOT NULL,
+    FOREIGN KEY (filme_categoria) REFERENCES categoria (categoria_nome),
+    FOREIGN KEY (filme_ator) REFERENCES ator (ator_id)
+);
+
+-- TODO: corrigir a chave estrangeira na tabela Filme

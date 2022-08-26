f971583cffa13f921c606e80c04eccde73d97bae
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-08-26 19:44:07 -0300

    fix: removido, até então, chaves primarias/estrangeiras

--- scripts/lista_1/universidade.sql
+++ scripts/lista_1/universidade.sql
@@ -5,6 +5,7 @@ DROP DATABASE IF EXISTS universidade;
 CREATE DATABASE universidade;
 
 -- Passo 1: Mapear Conjuntos de Entidades Regulares
+USE universidade;
 
 -- Criar tabela Pessoa
 CREATE TABLE pessoa (
@@ -14,35 +15,48 @@ CREATE TABLE pessoa (
     unome VARCHAR(256) NOT NULL,
     sexo VARCHAR(256) NOT NULL,
     data_nasc DATE NOT NULL,
-    endereco INT NOT NULL, -- relacionar com endereco
-    nome_pessoa INT NOT NULL, -- relacionar com nome_pessoa
-    endereco_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     rua VARCHAR(256) NOT NULL,
     cidade VARCHAR(256) NOT NULL,
     estado VARCHAR(256) NOT NULL,
     cep VARCHAR(256) NOT NULL,
-    numero INT NOT NULL,
-
-)
+    numero INT NOT NULL);
 
 -- Criar tabela Faculdade
 CREATE TABLE faculdade (
     faculdade_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     fnome VARCHAR(256) NOT NULL,
     reitor INT NOT NULL, -- relacionar com pessoa
-    escritorio_id INT NOT NULL, -- relacionar com escritorio
-)
+    escritorio_id INT NOT NULL -- relacionar com escritorio
+);
 
 -- Passo 2: Mapear entidades fracas
+
+-- Criar tabela Docente
+CREATE TABLE docente (
+    docente_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    pessoa_cpf INT NOT NULL, -- relacionar com pessoa
+    doc_escritorio INT NOT NULL, -- relacionar com escritorio
+    doc_telefone INT NOT NULL, -- relacionar com telefone
+    salario INT NOT NULL
+);
+
+-- Criar tabela Aluno
+CREATE TABLE aluno (
+    aluno_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    pessoa_cpf INT NOT NULL, -- relacionar com pessoa
+    tipo_aluno VARCHAR(256) NOT NULL,
+    departamento_principal_id INT NOT NULL, -- relacionar com departamento
+    departamento_secundario_id INT NOT NULL -- relacionar com departamento
+
+);
 
 -- Criar tabela Disciplina
 CREATE TABLE disciplina (
     disciplina_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     ddnome VARCHAR(256) NOT NULL,
     ddesc VARCHAR(256) NOT NULL,
-    departamento_id INT NOT NULL, -- relacionar com departamento
-    FOREIGN KEY (departamento_id) REFERENCES departamento (departamento_id)
-)
+    departamento_id INT NOT NULL -- relacionar com departamento
+);
 
 -- Criar tabela Departamento
 CREATE TABLE departamento (
@@ -50,5 +64,13 @@ CREATE TABLE departamento (
     dnome VARCHAR(256) NOT NULL,
     ddesc VARCHAR(256) NOT NULL,
     faculdade_id INT NOT NULL, -- relacionar com faculdade
-    FOREIGN KEY (faculdade_id) REFERENCES faculdade (faculdade_id)
-)
+    diretor_docente_id INT NOT NULL
+);
+
+-- Criar tabela Turma
+CREATE TABLE turma (
+    num_turma INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ano INT NOT NULL,
+    trim INT NOT NULL,
+    disciplina_id INT NOT NULL -- relacionar com disciplina
+);
